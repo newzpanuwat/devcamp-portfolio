01dd6711196ccaf5021c24a3ea13360201e5cb16
newzpanuwat <pfcreatorit@gmail.com>
2017-04-08 19:25:01 +0700

    Implement and  edit

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,18 +8,36 @@ class PortfoliosController < ApplicationController
     end
 
     def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path , notice: 'Portfolio was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+       
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      
       end
     end
   end
+
+    def edit
+        @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def update
+      @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path , notice: 'Portfolio was successfully updated.' }
+      
+      else
+        format.html { render :edit }
+     
+      end
+    end
+  end
+
 
 
 end

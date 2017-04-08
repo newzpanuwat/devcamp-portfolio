0ecb226e5f457b6d0c55ce12a14deaae22153fea
newzpanuwat <pfcreatorit@gmail.com>
2017-04-08 14:56:52 +0700

    Created seeds file for simple data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    )
+end
+
+puts "10 blog posts created!!!"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Ruby On Rails #{skill}",
+        percent_utilized: 60
+
+    )
+end
+
+puts "5 skilss created!!!"
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "My greate service",
+        body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+        main_image:"https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97200&w=350&h=200" ,
+        thumb_images: "https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97200&w=350&h=200" ,
+
+    )
+end
+
+
+
+
+
+puts " 9 portfolio created !!"
